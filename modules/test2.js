e234591d8545e6e33b1f6983b77e7006e5597fcb
Rémy Rakic <remy.rakic@gmail.com>
2014-06-23 17:35:27 +0200

    Module test implementation using the javax.script API + JDK8's Nashorn js engine

---
+++ modules/test2.js
@@ -0,0 +1,30 @@
+// local state
+var state = "new";
+
+function init() {
+	// perform initialization
+	state = "initialized";
+}
+
+function destroy() {
+	// perform un-initialization
+	state = "destroyed";
+	console.print ('Destroying module test2');
+}
+
+function test() {
+	memory.dump();
+	console.print(state);
+	console.logError("oh no!");
+}
+
+function dump() {
+	memory.dump();
+}
+
+// Useless Java integration example: exiting the interpreter process
+function exit() {
+	console.print("Exiting from module");
+	Java.type('java.lang.System').exit (0);
+}
+
--- src/Module.java
+++ src/Module.java
@@ -1,20 +1,14 @@
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.nio.charset.Charset;
-import java.nio.file.FileSystems;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import org.apache.commons.lang3.StringUtils;
-import org.mozilla.javascript.Context;
-import org.mozilla.javascript.Function;
-import org.mozilla.javascript.Scriptable;
-import org.mozilla.javascript.ScriptableObject;
+import java.io.FileReader;
+
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 
 public class Module {
 	private String name, filename;
-	private Context scriptContext;
-	private Scriptable scriptScope;
+	private ScriptEngine engine;
 	
 	public Module(String name, String filename) {
 		this.name = name;
@@ -22,48 +16,21 @@ public class Module {
 	}
 	
 	public void initialize(Memory memory) {
-		Path path = FileSystems.getDefault().getPath(".", filename);
-    	String source = null;
-		try {
-			source = StringUtils.join(Files.readAllLines(path, Charset.defaultCharset()), "\n");
-		} catch (IOException e) {
-			throw new RuntimeException("Could not load module script for '" + name + "' at " + path, e);
-		}
-		
-		scriptContext = Context.enter();
-		scriptScope = scriptContext.initStandardObjects();
-		
-		// init other global objects
-		try {
-			ScriptableObject.defineClass(scriptScope, Memory.class);
-			ScriptableObject.defineClass(scriptScope, Console.class);
-			
-		} catch (IllegalAccessException | InstantiationException | InvocationTargetException e) {
-			throw new RuntimeException("Runtime error", e);
-		}
+	    engine = new ScriptEngineManager().getEngineByName ("nashorn");
+	    
 
-		scriptContext.evaluateString(scriptScope, source, filename, 1, null);
-		
-		// set global objects
-		if (memory.getPrototype() == null) {
-			Scriptable defaultValue = scriptContext.newObject(scriptScope, "Memory");
-			memory.setPrototype(defaultValue.getPrototype());
-		}
-		scriptScope.put("memory", scriptScope, memory);
-		
-		if (Console.Instance.getPrototype() == null) {
-			Scriptable defaultValue = scriptContext.newObject(scriptScope, "Console");
-			Console.Instance.setPrototype(defaultValue.getPrototype());
-		}
-		scriptScope.put("console", scriptScope, Console.Instance);
-		
-		// call init function
-		Object fObj = scriptScope.get("init", scriptScope);
-		if (fObj instanceof Function) {
-		    Object functionArgs[] = { };
-		    Function f = (Function) fObj;
-		    f.call(scriptContext, scriptScope, scriptScope, functionArgs);
-		}
+	    engine.put ("memory", memory);
+	    engine.put ("console", Console.Instance);
+	    
+	    try {
+            engine.eval (new FileReader (this.filename));
+        }
+        catch (Exception e) {
+            System.err.printf ("Unexpected exception caught while evaluation module '%s' javascript file '%s': %s\n", name, filename, e);
+//            e.printStackTrace();
+        }
+	    
+	    invoke ("init");
 	}
 	
 	public void reload(Memory memory) {
@@ -74,50 +41,47 @@ public class Module {
 	public void evaluate(Statement statement, Iterable<String> operations) {
 		for (Value v : statement.getValues()) {
 			String name = v.name.toString();
-			if (v.value instanceof Integer)
-				ScriptableObject.putProperty(scriptScope, name, (int) v.value);
-			else if (v.value instanceof String)
-				ScriptableObject.putProperty(scriptScope, name, (String) v.value);
+			
+			if (v.value instanceof Integer || v.value instanceof String)
+			    engine.put (name, v.value);
 			else if (v.value instanceof Value[]) {
-				ValueCollection vc = new ValueCollection((Value[]) v.value);
-				if (vc.isArray()) 
-					ScriptableObject.putProperty(scriptScope, name, Context.javaToJS(vc.asArray(), scriptScope));
-				else
-				{
-					Object[] arg = { vc.values };
-					Scriptable jsVC = scriptContext.newObject(scriptScope, "ValueCollection", arg);
-					scriptScope.put(name, scriptScope, jsVC);
-				}
+			    ValueCollection vc = new ValueCollection((Value[]) v.value);
+			    if (vc.isArray())
+			        engine.put (name, vc.asArray());
+			    else
+			        engine.put (name, new ValueCollection (vc.values));
 			}
 			else
-				throw new RuntimeException("Value of type " + v.value.getClass().getSimpleName() + " is not supported");
+			    throw new RuntimeException("Value of type " + v.value.getClass().getSimpleName() + " is not supported");
 		}
 
 		if (!statement.testConditions())
 			return;
 		
 		for (String o : operations)	{
-			Object fObj = scriptScope.get(o, scriptScope);
-			if (!(fObj instanceof Function)) {
-			    System.err.println(o + " is undefined or not a function for module <" + name + ">.");
-			} else {
-			    Object functionArgs[] = { };
-			    Function f = (Function) fObj;
-			    f.call(scriptContext, scriptScope, scriptScope, functionArgs);
-			}
+		    invoke (o, o + " is undefined or not a function for module <" + name + ">.");
 		}
 	}
 	
 	public void close() {
-		// call init function
-		Object fObj = scriptScope.get("destroy", scriptScope);
-		if (fObj instanceof Function) {
-		    Object functionArgs[] = { };
-		    Function f = (Function) fObj;
-		    f.call(scriptContext, scriptScope, scriptScope, functionArgs);
-		}
-		
-		Context.exit();
+        invoke ("destroy");
+	}
+	
+	private void invoke (String function) {
+	    invoke (function, null);
+	}
+	
+	private void invoke (String function, String errorMessage) {
+	    try {
+            ((Invocable) engine).invokeFunction (function);
+        }
+	    catch (ScriptException e) {
+	        e.printStackTrace ();
+	    }
+        catch (Exception  e) {
+            if (errorMessage != null)
+                System.err.println (errorMessage);
+        }
 	}
 	
 	public String getName() {
